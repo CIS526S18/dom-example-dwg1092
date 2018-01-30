c9a5df4cbd9e9e11305fe2bd853c6e4f5570fd72
dwg1092 <dwg1092@gmail.com>
2018-01-30 12:45:50 -0600

    Add files via upload

---
+++ server.js
@@ -0,0 +1,40 @@
+"use strict";
+
+//Import the HTTP library
+const http = require('http');
+
+const PORT = 3000;
+
+//Import the fs library
+const fs = require('fs');
+
+function handleRequest(req, res) {
+	console.log(req.url);
+	
+	switch(req.url){
+		case '/':
+		case'/openhouse.html':
+			res.end(fs.readFileSync('public/openhouse.html'));
+			break;
+		case '/openhouse.css':
+			res.end(fs.readFileSync('public/openhouse.css'));
+			break;
+		case '/openhouse.js':
+			res.end(fs.readFileSync('public/openhouse.js'));
+			break;
+		default:
+			res.statusCode = 404;
+			res.end("File Not Found");
+	
+	}
+	
+}
+
+// Create the web server
+var server = http.createServer(handleRequest);
+
+//Start listening on port 80
+server.listen(PORT, function(){
+	console.log("Listening on port " + PORT);
+});
+
